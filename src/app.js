5ba4c16bb584577fa639f1fdede643d6af64db5e
yersson <yersson2@gmail.com>
2020-07-31 16:38:54 -0500

    second commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 
+// settings
+app.set('port', process.env.PORT || 4000);
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.get('/api/users', (req, res) => res.send('Users Routers'));
+app.get('/api/notes', (req, res) => res.send('Notes Routers'));
 
 module.exports = app;
 
